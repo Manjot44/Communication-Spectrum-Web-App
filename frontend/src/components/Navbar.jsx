9c0e482239ed1db5487af39ebe8bc4599010eeda
Jaden Cheng <z5420209@unsw.edu.au>
2024-10-11 02:05:22 +1100

    Homepage basic design
    Adding a basic homepage design with essential UI elements

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Navbar.css";
+
+// Image assets
+// Logos
+import Logo from "../assets/mycomms_white.png";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src={Logo} alt="MyComms logo" className="logo-img" />
+      </div>
+      <div className="navbar-links">
+        <a href="home" className="nav-item">
+          Home
+        </a>
+        <a href="explore" className="nav-item">
+          Explore
+        </a>
+        <a href="gallery" className="nav-item">
+          Gallery
+        </a>
+        <a href="templates" className="nav-item">
+          Templates
+        </a>
+        <a href="learn" className="nav-item">
+          Learn
+        </a>
+        <a href="categories" className="nav-item">
+          Categories
+        </a>
+      </div>
+      <div className="profile-switch">
+        <button className="profile-btn">👤 Switch Profile</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
