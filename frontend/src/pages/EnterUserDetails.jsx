5806dd7dbf7a09ef8dd29077b4a7c081a0e95d4b
Jerry Lin <z5363361@ad.unsw.edu.au>
2024-10-11 09:14:27 +1100

    added back a missing function

--- frontend/src/pages/EnterUserDetails.jsx
+++ frontend/src/pages/EnterUserDetails.jsx
@@ -22,6 +22,14 @@ function AddUser({ token, setTokenFunc }) {
   let currentData = ""; // Variable to store existing data (if needed)
   let store = ""; // Placeholder for updated data to be sent back
 
+  // Submits the register form when the enter key is pressed in any of the fields
+  function handleKeyDown (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        handleCreateButton();
+    }
+  }
+  
   // Handle profile picture upload
   const handleProfilePictureUpload = (event) => {
     const file = event.target.files[0];
