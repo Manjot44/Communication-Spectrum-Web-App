085f7edf72992f717a5fcc4556b93813c0fffdbc
Jerry Lin <z5363361@ad.unsw.edu.au>
2024-10-11 11:37:31 +1100

    standardised tabs to 2 spaces, and cleaned up unecessary code

--- frontend/src/components/TextFieldComponent.jsx
+++ frontend/src/components/TextFieldComponent.jsx
@@ -2,19 +2,19 @@ import TextField from '@mui/material/TextField';
 import '../App.css'
 
 const TextFieldComponent = ({ label, value, onChange, onKeyDown, type }) => {
-    return (
-        <>
-            <TextField
-                className='login-input-box'
-                label={label}
-                variant="outlined"
-                type={type}
-                value={value}
-                onChange={onChange}
-                onKeyDown={onKeyDown}
-            />
-        </>
-    );
+  return (
+    <>
+      <TextField
+        className='login-input-box'
+        label={label}
+        variant="outlined"
+        type={type}
+        value={value}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+      />
+    </>
+  );
 };
 
 export default TextFieldComponent;
