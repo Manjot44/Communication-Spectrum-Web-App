401fb4c34892d5fa31d9a9bb58bccd3ded146048
Jerry Lin <z5363361@ad.unsw.edu.au>
2024-10-28 15:54:25 +1100

    A bunch of new stuff

---
+++ frontend/src/components/TaskAnalysesStep.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Grid from '@mui/material/Grid2';
+import Box from '@mui/material/Box';
+import {
+  Card,
+  Typography,
+  CardContent,
+  Button,
+  TextField,
+  Avatar,
+} from "@mui/material";
+import VisualSupportImage from './VisualSupportImage';
+
+function TaskAnalysesStep ({ image, setImage }) {
+	// const [image, setImage] = useState(null);
+
+	// // Handle Image Change in Visual Support Options box
+	// const handleImageChange = (event) => {
+  //   const file = event.target.files[0];
+  //   if (file) {
+  //     const reader = new FileReader();
+  //     reader.onloadend = () => {
+  //       setImage(reader.result);
+  //     };
+  //     reader.readAsDataURL(file);
+  //   }
+  // };
+
+  // const handleDivClick = () => {
+  //   document.getElementById('fileInput').click();
+  // };
+
+	return (
+		<>
+			<Grid container direction="column" spacing={0} style={{ margin: '10px' }}>
+				<Grid item xs={12}>
+          <Box sx={{ height: '50px', width: '350px', color: 'white', display: 'flex', fontFamily: 'Poppins', fontWeight: 'bold', justifyContent: 'center', alignItems: 'center', backgroundColor: '#000CA4', borderRadius: '15px 15px 0 0' }}>
+            Step 1
+          </Box>
+        </Grid>
+				<Grid item xs={12} style={{ backgroundColor: 'white', width: '350px', height: '350px', padding: '15px' }}>
+          {/* <div style={{ border: '1px dotted black', height: '100%', width: '100%' }}>
+						<img
+							alt=""
+							className="profile-picture"
+						/>
+					</div> */}
+					{/* <VisualSupportImage style={{ height: '100%', width: '100%' }}/> */}
+
+					<VisualSupportImage image={image} setImage={setImage}/>
+					{/* <VisualSupportImage /> */}
+
+        </Grid>
+        <Grid item xs={12}>
+          <Box sx={{ height: '150px', width: '350px', color: 'black', fontFamily: 'Poppins', fontWeight: 'bold', backgroundColor: 'white', padding: '0 15px', borderRadius: '0 0 15px 15px' }}>
+					<TextField
+						label="Step Name"
+						variant="outlined"
+						fullWidth
+						style={{
+							marginBottom: "20px",
+							backgroundColor: "white",
+						}}
+					/>
+					<br />
+					<TextField
+						label="Timer"
+						variant="outlined"
+						fullWidth
+						style={{
+							marginBottom: "20px",
+							backgroundColor: "white",
+						}}
+					/>
+          </Box>
+        </Grid>
+      </Grid>
+		</>
+	);
+}
+
+export default TaskAnalysesStep;
---
+++ frontend/src/pages/CreateTaskAnalysesScratch.jsx
@@ -0,0 +1,177 @@
+import React, { useState, useEffect } from 'react';
+import Navbar from "../components/Navbar";
+import { useParams, Link } from "react-router-dom";
+import {
+	Grid,
+  Card,
+  Typography,
+  CardContent,
+  Button,
+  TextField,
+  Avatar,
+} from "@mui/material";
+// import Grid from '@mui/material/Grid2';
+import '../App.css';
+import PhotoCameraBackIcon from '@mui/icons-material/PhotoCameraBack';
+import FormatColorTextIcon from '@mui/icons-material/FormatColorText';
+import TaskAnalysesStep from '../components/TaskAnalysesStep';
+import VisualSupportImage from '../components/VisualSupportImage';
+
+function CreateTaskAnalysesScratch({ token, setTokenFunc }) {
+  const { profileID } = useParams();
+	const [text, setText] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+	const [image, setImage] = useState(null);
+	const [stepImages, setStepImages] = useState([]);  // Array to store images for each step
+
+	// // Handle Image Change in Visual Support Options box
+	// const handleImageChange = (event) => {
+  //   const file = event.target.files[0];
+  //   if (file) {
+  //     const reader = new FileReader();
+  //     reader.onloadend = () => {
+  //       setImage(reader.result);
+  //     };
+  //     reader.readAsDataURL(file);
+  //   }
+  // };
+
+  // const handleDivClick = () => {
+  //   document.getElementById('fileInput').click();
+  // };
+
+	// Function to update image for a specific step
+  const handleStepImageChange = (index, newImage) => {
+    setStepImages((prev) => {
+      const updatedImages = [...prev];
+      updatedImages[index] = newImage;
+      return updatedImages;
+    });
+  };
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const toggleEditing = () => {
+    setIsEditing(!isEditing);
+  };
+
+  return (
+    <>
+      <link
+        href="https://fonts.googleapis.com/css?family=Poppins"
+        rel="stylesheet"
+      ></link>
+      <Navbar profileID={profileID}/>
+      <br />
+      <Typography variant="h4" align="center" gutterBottom>
+				{isEditing ? (
+					<input
+						type="text"
+						value={text}
+						onChange={handleTextChange}
+						onBlur={toggleEditing} // Stop editing when input loses focus
+						autoFocus
+					/>
+				) : (
+					<b onClick={toggleEditing} style={{ cursor: 'pointer' }}>
+						{text || <span style={{ color: 'grey' }}>Insert Task Name Here</span>}
+					</b>
+				)}
+      </Typography>
+      <br />
+			<div class='page-wrapper-style' style={{ padding: '0 50px' }}>
+				<Grid container spacing={3}>
+					<Grid item xs={12} md={2.5}>
+						<Card class='task-analyses-create-options' style={{ height: '80vh' }}>
+							<CardContent>
+								<Typography variant="h5" component="div" style={{ fontFamily: 'Poppins' }}>
+									<b>Visual Support Options</b>
+								</Typography>
+								<br />
+								<TextField
+										label="Search Assets/Shapes"
+										variant="filled"
+										fullWidth
+										style={{
+											marginBottom: "20px",
+											backgroundColor: "white",
+										}}
+								/>
+								<br/>
+
+								<VisualSupportImage image={image} setImage={setImage} />
+								{/* <VisualSupportImage /> */}
+								<br />
+								<Button 
+									variant="contained"
+									style={{ width: '100%', height: '10vh', backgroundColor: '#6b4bef', fontFamily: 'Poppins' }}
+								>
+									<Grid container spacing={1}>
+										<Grid item xs={12} md={4}>
+											<PhotoCameraBackIcon style={{ height: '7vh', width: '7vh' }}/>
+										</Grid>
+										<Grid item xs={12} md={8} class="d-flex align-items-center justify-content-center">
+											<h5>Pick From Gallery</h5>
+										</Grid>
+									</Grid>
+								</Button>
+								<br />
+								<br />
+								<Button 
+									variant="contained"
+									style={{ width: '100%', height: '10vh', backgroundColor: '#6b4bef', fontFamily: 'Poppins' }}
+								>
+									<Grid container spacing={1}>
+										<Grid item xs={12} md={4}>
+											<FormatColorTextIcon style={{ height: '7vh', width: '7vh' }}/>
+										</Grid>
+										<Grid item xs={12} md={8} class="d-flex align-items-center justify-content-center">
+											<h5>Insert Text</h5>
+										</Grid>
+									</Grid>
+								</Button>
+								<br/>
+								<br/>
+								<Button 
+									variant="contained"
+									style={{ width: '100%', backgroundColor: '#26c3ba', fontFamily: 'Poppins' }}
+								>
+									Create Visual Support
+								</Button>
+							</CardContent>
+						</Card>
+					</Grid>
+					<Grid item xs={12} md={9.5}>
+						<Card class='task-analyses-create-options' style={{ backgroundColor: '#f0f0f0', height: '80vh' }}>
+							<CardContent>
+								<Typography variant="h5" component="div" style={{ fontFamily: 'Poppins' }}>
+									<h4 style={{ fontFamily: 'Poppins', color: 'black' }}>
+										<b>Task Steps</b>
+									</h4>
+									<div style={{ height: '70vh', color: 'black', backgroundColor: 'transparent', overflowY: 'scroll', display: 'flex', flexWrap: 'wrap' }}>
+										{/* <TaskAnalysesStep />
+										<TaskAnalysesStep />
+										<TaskAnalysesStep />
+										<TaskAnalysesStep />
+										<TaskAnalysesStep /> */}
+										{[...Array(5)].map((_, index) => (
+                      <TaskAnalysesStep
+                        key={index}
+                        image={stepImages[index] || null}
+                        setImage={(newImage) => handleStepImageChange(index, newImage)}
+                      />
+                    ))}
+									</div>
+								</Typography>
+							</CardContent>
+						</Card>
+					</Grid>
+				</Grid>
+      </div>
+    </>
+  );
+}
+
+export default CreateTaskAnalysesScratch;
