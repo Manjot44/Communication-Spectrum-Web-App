b20d5cb92ca8d28e25d80328cf7e11c2a89f0fb6
Sanjam <sanjamsingh@student.unsw.edu.au>
2024-11-01 00:06:42 +1100

    Made new components and for Arrow, Frist and Then Steps, and created the First-Then page

---
+++ frontend/src/components/Arrow.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Arrow() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      margin: '0 30px',
+    }}>
+      <div style={{
+        width: '200px',
+        height: '12px',
+        backgroundColor: '#FF7C33', 
+        position: 'relative',
+      }}>
+        <div style={{
+          width: '0', 
+          height: '0', 
+          borderTop: '20px solid transparent',
+          borderBottom: '20px solid transparent',
+          borderLeft: '20px solid #FF7C33',
+          position: 'absolute',
+          right: '-20px',
+          top: '-14px',
+        }}></div>
+      </div>
+    </div>
+  );
+}
+
+export default Arrow;
