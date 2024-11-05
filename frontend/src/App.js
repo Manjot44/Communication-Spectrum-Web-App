8fb5f9f971ed09c189a9e907ec7012162043602b
Jerry Lin <z5363361@ad.unsw.edu.au>
2024-11-05 22:11:35 +1100

    Made the page where you choose what kind of template you want a component, made the orange buttons on this page a separate component as well.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,7 @@ import FirstThen from './pages/FirstThen.jsx';
 import CreateTaskAnalysesScratch from './pages/CreateTaskAnalysesScratch.jsx';
 import CreateChoiceBoard from './pages/CreateChoiceBoard.jsx';
 import CreateDailySchedulesScratch from './pages/CreateDailySchedulesScratch.jsx';
+import TemplateChoice from './pages/TemplateChoice.jsx';
 
 // ProtectedRoute component to handle route protection
 const ProtectedRoute = ({
@@ -91,12 +92,10 @@ function App() {
           <Route path="/choiceboards/:profileID" element={<ProtectedRoute element={ChoiceBoards} isAuth={isAuth} token={token} setTokenFunc={updateToken} />} />
           <Route path="/createChoiceBoard/:profileID" element={<ProtectedRoute element={CreateChoiceBoard} isAuth={isAuth} token={token} setTokenFunc={updateToken} />} />
           <Route path="/firstthen/:profileID" element={<ProtectedRoute element={FirstThen} isAuth={isAuth} token={token} setTokenFunc={updateToken} />} />
-          <Route path="/createTaskAnalysesScratch/:profileID" element={<ProtectedRoute element={CreateTaskAnalysesScratch} isAuth={isAuth} token={token} setTokenFunc={updateToken} />} />
-          <Route path="/createDailySchedulesScratch/:profileID" element={<ProtectedRoute element={CreateDailySchedulesScratch} isAuth={isAuth} token={token} setTokenFunc={updateToken} />} />
+          <Route path="/chooseTemplate/:profileID" element={<ProtectedRoute element={TemplateChoice} isAuth={isAuth} token={token} setTokenFunc={updateToken} />} />
         </Routes>
       </BrowserRouter>
     </LocalizationProvider>
-    
   );
 }
 
--- frontend/src/components/VisualSupportTypes.jsx
+++ frontend/src/components/VisualSupportTypes.jsx
@@ -17,6 +17,11 @@ import { useNavigate } from "react-router-dom";
 
 function VisualSupportTypes({ profileID }) {
 	const navigate = useNavigate();
+  const taskAnalyses = { title: "Create New Task Analyses", scratch: `/taskanalyses/${profileID}`, temp: `/taskanalyses/${profileID}`, exist: `/taskanalyses/${profileID}` };
+  const socialStory = { title: "Create New Social Story", scratch: `/socialstories/${profileID}`, temp: `/socialstories/${profileID}`, exist: `/socialstories/${profileID}` };
+  const envirSupport = { title: "Create New Environment Support", scratch: `/envirsupports/${profileID}`, temp: `/envirsupports/${profileID}`, exist: `/envirsupports/${profileID}` };
+  const choiceBoard = { title: "Create New Choice Board", scratch: `/choiceboards/${profileID}`, temp: `/choiceboards/${profileID}`, exist: `/choiceboards/${profileID}` };
+  const firstThen = { title: "Create New First-Then", scratch: `/firstthen/${profileID}`, temp: `/firstthen/${profileID}`, exist: `/firstthen/${profileID}` };
 
   return (
 		<>
@@ -25,7 +30,7 @@ function VisualSupportTypes({ profileID }) {
         <br />
         <Grid container spacing={2} justifyContent="space-around">
           <Grid item>
-            <Button variant="contained" class='circular-icon-style' onClick={async () => {navigate(`/taskanalyses/${profileID}`)}}>
+            <Button variant="contained" class='circular-icon-style' onClick={async () => {navigate(`/chooseTemplate/${profileID}`, { state: taskAnalyses })}}>
               <Task fontSize="large" />
             </Button>
             <Typography align="center" style={{ fontFamily: "Poppins", color: "#6B4BEF" }}>Task Analyses</Typography>
@@ -43,25 +48,25 @@ function VisualSupportTypes({ profileID }) {
             <Typography align="center" style={{ fontFamily: "Poppins", color: "#6B4BEF" }}>Weekly Calendars</Typography>
           </Grid>
           <Grid item>
-            <Button variant="contained" class='circular-icon-style' onClick={async () => {navigate(`/socialstories/${profileID}`)}}>
+            <Button variant="contained" class='circular-icon-style' onClick={async () => {navigate(`/chooseTemplate/${profileID}`, { state: socialStory })}}>
               <Group fontSize="large" />
             </Button>
             <Typography align="center" style={{ fontFamily: "Poppins", color: "#6B4BEF" }}>Social Stories</Typography>
           </Grid>
           <Grid item>
-            <Button variant="contained" class='circular-icon-style' onClick={async () => {navigate(`/envirsupports/${profileID}`)}}>
+            <Button variant="contained" class='circular-icon-style' onClick={async () => {navigate(`/chooseTemplate/${profileID}`, { state: envirSupport })}}>
               <Warning fontSize="large" />
             </Button>
             <Typography align="center" style={{ fontFamily: "Poppins", color: "#6B4BEF" }}>Environmental Supports</Typography>
           </Grid>
           <Grid item>
-            <Button variant="contained" class='circular-icon-style' onClick={async () => {navigate(`/choiceboards/${profileID}`)}}>
+            <Button variant="contained" class='circular-icon-style' onClick={async () => {navigate(`/chooseTemplate/${profileID}`, { state: choiceBoard })}}>
               <CheckBox fontSize="large" />
             </Button>
             <Typography align="center" style={{ fontFamily: "Poppins", color: "#6B4BEF" }}>Choice Boards</Typography>
           </Grid>
           <Grid item>
-            <Button variant="contained" class='circular-icon-style' onClick={async () => {navigate(`/firstthen/${profileID}`)}}>
+            <Button variant="contained" class='circular-icon-style' onClick={async () => {navigate(`/chooseTemplate/${profileID}`, { state: firstThen })}}>
               <Checklist fontSize="large" />
             </Button>
             <Typography align="center" style={{ fontFamily: "Poppins", color: "#6B4BEF" }}>First-Then</Typography>
