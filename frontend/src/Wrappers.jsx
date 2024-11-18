3621a2dc3400feccbeabd128dced19acfc0c98ed
Jerry Lin <z5363361@ad.unsw.edu.au>
2024-11-18 23:55:51 +1100

    forgor to add these too

--- frontend/src/Wrappers.jsx
+++ frontend/src/Wrappers.jsx
@@ -4,7 +4,8 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import CancelIcon from '@mui/icons-material/Cancel';
 import Accordion from '@mui/material/Accordion';
-import { TextField, Typography, Card, Button, Stack, Box } from "@mui/material";
+import { TextField, Typography, Card, Button, Stack, Box, Checkbox } from "@mui/material";
+import { Link } from "react-router-dom";
 import "@fontsource/poppins";
 
 export const PageWrapperStyle = styled('div')({
@@ -51,6 +52,13 @@ export const SnapShotStyleCard = styled(Card)({
   borderRadius: "15px",
   color: "#000CA4",
   padding: "20px",
+});
+
+export const SnapshotStyleDiv = styled('div')({
+  padding: '20px',
+  backgroundColor: '#f0f0f0',
+  borderRadius: '15px',
+  color: '#000ca4',
 });
 
 export const InnerSnapShotBox = styled('div')({
@@ -261,7 +269,7 @@ export const UserManageOptions = styled(Box)({
   justifyContent: "center",
   alignItems: "center",
   marginTop: '16px', /* equivalent to mt={2} */
-  gap: '16px', /* equivalent to gap={2} */
+  gap: '16px',       /* equivalent to gap={2} */
 });
 
 // ViewSupport
@@ -277,4 +285,242 @@ export const ViewSupportCard = styled(Card)({
   backgroundColor: 'white',
   height: '87vh',
   width: '100%'
+});
+
+// Modals
+export const ModalBox = styled(Box)({
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  backgroundColor: 'white',
+  padding: '25px',
+  boxShadow: 24,
+  p: 4,
+});
+
+export const ChooseFromGalleryBox = styled(Box)({
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "63%",
+  height: "80%",
+  backgroundColor: "white",
+  boxShadow: 24,
+  p: 4,
+  borderRadius: "8px",
+  textAlign: "center",
+  padding: "20px"
+});
+
+// General Wrapper
+export const BlueColour = styled('div')({
+  color: "#000CA4"
+});
+
+export const ButtonsBox = styled(Box)({
+  display: "flex",
+  justifyContent: "space-around",
+  marginTop: 10
+});
+
+// FromScratch
+export const FromScratchToolBox = styled(Box)({
+  display: "flex",                          // Make it a flex container
+  flexDirection: "row",                     // Horizontal layout
+  alignItems: "center",                     // Vertically center the content
+  justifyContent: "space-evenly",           // Evenly space buttons
+  height: "8vh",                            // Set toolbar height
+  backgroundColor: "white",                 // Optional: Set background color
+  boxShadow: "0px 2px 4px rgba(0,0,0,0.1)", // Optional: Add a shadow
+  padding: "0 16px",                        // Add horizontal padding
+  overflowX: "auto",                        // Allow horizontal scrolling if content overflows
+});
+
+
+//NavBar
+export const NavBar = styled('div')({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  backgroundColor: '#000ca4',
+  padding: '10px 20px',
+  color: 'white',
+  fontFamily: "Poppins"
+});
+
+export const LogoBox = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+});
+
+export const NavbarLink = styled(Link)({
+  margin: '0 10px',
+  color: 'white',
+  textDecoration: 'none',
+});
+
+export const NavbarLinks = styled('div')({
+  display: 'flex'
+});
+
+export const SwitchProfileButton = styled(Button)({
+  backgroundColor: '#00c89c',
+  color: 'white',
+  padding: '8px 12px',
+  border: 'none',
+  borderRadius: '5px',
+  cursor: 'pointer',
+  "&:hover": {
+    backgroundColor: "#09a281",
+  },
+});
+
+// Professional and User Profile Boxes (for sharing)
+export const ProfileCheckBox = styled(Checkbox)({
+  position: "absolute",
+  bottom: 0,
+  right: 8,
+  color: "#ff7c33",
+  "&.Mui-checked": {
+    color: "#ff7c33",
+  },
+});
+
+// RecentSupports
+export const RecentSupportsCard = styled(Card)({
+  padding: '10px',
+  marginBottom: '20px',
+  backgroundColor: 'white',
+  width: '100%',
+  textAlign: 'center',
+  border: '1px solid #000ca4',
+  color: '#000ca4',
+  borderRadius: '15px',
+  transition: 'transform 0.3s',
+  "&:hover": {
+    transform: "scale(1.05)",
+  },
+});
+
+export const RecentSupportImage = styled('div')({
+  position: "relative",
+  width: "100%",
+  paddingBottom: "56.25%", // 16:9 aspect ratio (adjust this if needed)
+  overflow: "hidden",
+  borderRadius: "10px",
+});
+
+export const RecentSupportImgTag = styled('img')({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  objectFit: "cover",
+  borderRadius: "5px",
+});
+
+export const RecentSupportEditBox = styled(Box)({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  marginTop: "10px",
+  position: "relative"
+});
+
+export const RecentSupprtTitle = styled(Typography)({
+  fontSize: "1.2rem",
+  textAlign: "center"
+});
+
+export const RecentSupportButton = styled(IconButton)({
+  color: "grey",
+  position: "absolute",
+  top: "50%",
+  transform: "translateY(-50%)",
+});
+
+export const NoSupportsTypography = styled(Typography)({
+  marginTop: "20px",
+  color: "#666",
+  textAlign: 'center'
+});
+
+// Left hand Menu in Editing Visual Support Pages
+export const LeftMenuCard = styled(Card)({
+  backgroundColor: 'white',
+  color: 'black',
+  borderRadius: '0px',
+  fontFamily: "Poppins",
+  height: '87.5vh',
+  boxShadow: '0 4px 8px 0 rgba(153, 153, 153, 0.2) 0 6px 20px 0 rgba(153, 153, 153, 0.2)',
+});
+
+export const CreateSupportButton = styled(Button)({
+  width: "100%",
+  backgroundColor: "#26c3ba",
+  fontFamily: "Poppins",
+});
+
+// Share Modal
+export const ShareModalBox = styled(Box)({
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "63%",
+  height: "70%",
+  backgroundColor: "white",
+  boxShadow: 24,
+  p: 4,
+  borderRadius: "8px",
+  textAlign: "center",
+  padding: 30
+});
+
+// User Profiles
+export const UserProfileCircleOuter = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  cursor: "pointer",
+  padding: 2,
+  borderRadius: "12px",
+  backgroundColor: "#f9f9f9",
+  boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
+  width: "200px",
+  height: "270px",
+  transition: "transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out",
+  "&:hover": {
+    transform: "scale(1.05)",
+    boxShadow: "0 8px 16px rgba(0, 112, 255, 0.5)",
+  },
+  position: "relative",
+});
+
+export const UserProfileCircle = styled(Box)({
+  width: "80%",
+  height: "auto",
+  borderRadius: "50%",
+  marginBottom: "12px",
+  border: "2px solid #ddd",
+});
+
+export const UserProfileName = styled(Typography)({
+  fontWeight: "bold",
+  fontFamily: "Poppins",
+  color: "#333",
+  textAlign: "center",
+  marginBottom: "auto",
+});
+
+export const UserProfileCircleButtonBox = styled(Box)({
+  display: "flex",
+  gap: 1,
+  justifyContent: "center",
+  marginTop: "auto",
+  paddingTop: "8px",
 });
